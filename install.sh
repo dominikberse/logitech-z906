eb47e0bf5047d9a30c75f69dc762a32641155d63
Dominik Berse <dominik.berse@tapdo.io>
2021-06-22 15:10:54 +0200

    fix: adjusting raspotify cache location

--- install.sh
+++ install.sh
@@ -82,6 +82,9 @@ sudo systemctl enable logitech
 whiptail --yesno "Install and configure raspotify?" 20 60
 if [ "$?" -eq 0 ] ; then
   (curl -sL https://dtcooper.github.io/raspotify/install.sh | sh)
+  sudo sed -i "s/^#\?CACHE_ARGS=.*$/CACHE_ARGS=\"--cache \/tmp\/raspotify\"/" /etc/default/raspotify
+
+  # set device name
   DEVNAME=$(whiptail --inputbox "Enter speaker name" 20 60 "raspotify" 3>&1 1>&2 2>&3)
   if [ $? -eq 0 ] ; then
     sudo sed -i "s/^#\?DEVICE_NAME=.*$/DEVICE_NAME=\"$DEVNAME\"/" /etc/default/raspotify
